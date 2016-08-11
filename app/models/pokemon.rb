97aab1e0c07166480e072eedb08e9bac80359710
kaiguogit <kaiguo1988@gmail.com>
2016-08-11 06:40:02 +0000

    added image_url

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,4 +3,7 @@ class Pokemon < ActiveRecord::Base
   belongs_to :listing
   belongs_to :listing_trade, class_name: :Listing, foreign_key: :pokemon_trade_id
   
+  def image_url
+    "https://img.pokemondb.net/artwork/#{name.downcase}.jpg"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811014843) do
+ActiveRecord::Schema.define(version: 20160811060729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160811014843) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image_url"
   end
 
   create_table "users", force: :cascade do |t|
