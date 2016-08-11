805c2e468b57f3b5dfaaae1e84f1492edfda3b86
KennethQiu <kennethqiujunhao@gmail.com>
2016-08-11 02:48:28 +0000

    added listing model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,6 @@
 class Pokemon < ActiveRecord::Base
   belongs_to :user
+  belongs_to :listing
+  belongs_to :listing_trade, class_name: :Listing, foreign_key: :pokemon_trade_id
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811014121) do
+ActiveRecord::Schema.define(version: 20160811014843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.integer  "pokemon_id"
+    t.integer  "user_id"
+    t.integer  "price"
+    t.string   "status"
+    t.integer  "pokemon_trade_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "pokemons", force: :cascade do |t|
     t.string   "name"
