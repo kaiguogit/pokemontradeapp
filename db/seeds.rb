0919b1b4f98a935b3c73c8a7e3bee370319d246c
kaiguogit <kaiguo1988@gmail.com>
2016-08-11 19:37:22 +0000

    created profile page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,10 @@ u = User.create(username: 'test', email: "test@email.com")
 u.password = 'test'
 u.save
 
-Pokemon.create(name:"Pikachu", cp: 100)
-Pokemon.create(name:"Wartortle", cp: 50)
+u.pokemons.create(name:"Pikachu", cp: 100)
+u.pokemons.create(name:"Wartortle", cp: 50)
+
+
 
 l =  Listing.new
 l.pokemon = Pokemon.first
