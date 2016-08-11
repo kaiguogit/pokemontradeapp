4e9aabeea94de40d2f336963bddaa170e39af240
kaiguogit <kaiguo1988@gmail.com>
2016-08-11 19:06:18 +0000

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,23 @@
+User.destroy_all
+Listing.destroy_all
+Pokemon.destroy_all
+
 u = User.create(username: 'test', email: "test@email.com")
 u.password = 'test'
 u.save
 
 Pokemon.create(name:"Pikachu", cp: 100)
 Pokemon.create(name:"Wartortle", cp: 50)
+
+l =  Listing.new
+l.pokemon = Pokemon.first
+l.pokemon_trade = Pokemon.last
+l.price = 1000
+l.save
+
+
+l =  Listing.new
+l.pokemon = Pokemon.last
+l.pokemon_trade = Pokemon.first
+l.price = 2000
+l.save
