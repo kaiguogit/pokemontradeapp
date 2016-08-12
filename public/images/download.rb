4fab6d70a08640e1315d49dfaba6e9fd91cd374f
kaiguogit <kaiguo1988@gmail.com>
2016-08-12 14:26:15 +0000

    downloaded pictures

---
+++ public/images/download.rb
@@ -0,0 +1,23 @@
+  def image_url(id)
+    # "https://img.pokemondb.net/artwork/#{name.downcase}.jpg"
+    "http://assets.pokemon.com/assets/cms2/img/pokedex/detail/#{id_to_index(id)}.png"
+  end
+
+  def id_to_index(number)
+    result = number.to_s
+    if result.length < 3
+      result = "0" * (3 - result.length) + result
+    else
+      result
+    end
+  end
+
+
+require "open-uri"
+(1..151).each do |a|
+  open("#{image_url(a)}") {|f|
+   File.open("#{id_to_index(a)}.png","wb") do |file|
+     file.puts f.read
+   end
+  }
+end
